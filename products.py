f7189caa24be28df86bbcf6fb4dbad6238338d7c
KC <kelvinchan981024@hotmail.com>
2020-06-17 01:27:36 +0800

    For loop + list

--- products.py
+++ products.py
@@ -1,12 +1,16 @@
 products = []
 while True:
-	name = input('Please enter a product name')
-	if name == 'q'
+	name = input('Please enter a product name: ')
+	if name == 'quit':
 		break
-	price = input('Please enter the price of product')
+	price = input('Please enter the price of product: ')
 	p = [name, price]
 	#p = []
 	#p.append(name)
 	#p.append(price)
 	products.append(p)
 print(products)
+
+for p in products:
+	print(p)
+	print(p[0], '的價格是', p[1])
